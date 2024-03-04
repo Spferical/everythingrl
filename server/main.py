a9e9e6d134cd40b8fec78765d5e303742e047c2a
Matthew Pfeiffer <spferical@gmail.com>
2024-03-04 14:59:23 -0500

    Split out ai code

--- server/main.py
+++ server/main.py
@@ -2,65 +2,8 @@
 import os
 
 import click
-import requests
 
-
-API_URL = "https://api.mistral.ai"
-API_KEY = os.getenv("AISTUDIO_API_KEY")
-
-
-def ask_mistral(messages):
-    model = "mistral-small"
-
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": f"Bearer {API_KEY}",
-    }
-
-    payload = {"model": "mistral-tiny", "messages": messages, "max_tokens": 300}
-
-    response = requests.post(
-        f"{API_URL}/v1/chat/completions", headers=headers, json=payload
-    )
-
-    response.raise_for_status()
-
-    return response.json()["choices"][0]["message"]["content"]
-
-
-def ask_google(prompt_parts: list[str]):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key={API_KEY}"
-    headers = {"Content-Type": "application/json"}
-    payload = {
-        "contents": [
-            {
-                "parts": [{"text": part} for part in prompt_parts],
-            }
-        ],
-        "generationConfig": {
-            "temperature": 0.9,
-            "topK": 1,
-            "topP": 1,
-            "maxOutputTokens": 2048,
-            "stopSequences": [],
-        },
-        "safetySettings": [
-            {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_ONLY_HIGH"},
-            {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_ONLY_HIGH"},
-            {
-                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                "threshold": "BLOCK_ONLY_HIGH",
-            },
-            {
-                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                "threshold": "BLOCK_ONLY_HIGH",
-            },
-        ],
-    }
-    response = requests.post(url, headers=headers, json=payload)
-    response.raise_for_status()
-    return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+import ai
 
 
 @click.group()
@@ -81,7 +24,7 @@ def craft(item1: str, item2: str):
         f"input: - {item1}\n- {item2}",
         "output: ",
     ]
-    print(ask_google(prompt_parts))
+    print(ai.ask_google(prompt_parts))
 
 
 @cli.command()
