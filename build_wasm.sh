788dce50d4225b43b17c66a48225d343a39bde8e
Matthew Pfeiffer <spferical@gmail.com>
2024-04-27 22:57:08 -0400

    Drop nix from build_wasm.sh

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-nix build '.#wasm'
-cp -f result/bin/seven-drl-2024.wasm dist/
+set -eo pipefail
+cargo build --release --target wasm32-unknown-unknown
+target_dir=$(cargo metadata --format-version=1 | jq -r .target_directory)
+root=$(cargo metadata --format-version=1 | jq -r .resolve.root)
+project_name=$(cargo metadata --format-version=1 \
+               | jq -r ".packages[] | select(.id==\"${root}\") | .name")
+cp -f "${target_dir}/wasm32-unknown-unknown/release/${project_name}.wasm" dist/
 rm -rf dist/assets/
 cp -r assets/ dist/assets/
