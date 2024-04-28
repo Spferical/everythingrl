d71e0bab6cbe5425c6c620f254d28f4e3f8578f2
Matthew Pfeiffer <spferical@gmail.com>
2024-04-28 00:37:18 -0400

    Add script to build all platforms

---
+++ build-all.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+target_dir=$(cargo metadata --format-version=1 | jq -r .target_directory)
+root=$(cargo metadata --format-version=1 | jq -r .resolve.root)
+project_name=$(cargo metadata --format-version=1 \
+               | jq -r ".packages[] | select(.id==\"${root}\") | .name")
+
+function build_linux() {
+    CARGO_TARGET_DIR=./.centos-target/ cross build --target=x86_64-unknown-linux-gnu --release
+    mkdir -p dist_linux
+    rsync -a assets/ dist_linux/assets/
+    cp "./.centos-target/x86_64-unknown-linux-gnu/release/${project_name}" dist_linux/
+}
+build_linux
+
+function build_windows() {
+    cross build --target=x86_64-pc-windows-gnu --release
+    rm -rf dist_windows
+    mkdir -p dist_windows
+    rsync -a assets/ dist_windows/assets/
+    cp "${target_dir}/x86_64-pc-windows-gnu/release/${project_name}.exe" dist_windows/
+}
+build_windows
+
+function build_wasm() {
+    devenv shell -- cargo build --release --target wasm32-unknown-unknown
+    cp -f "${target_dir}/wasm32-unknown-unknown/release/${project_name}.wasm" dist/
+    rm -rf dist/assets/
+    rsync -a assets/ dist/assets/
+}
+build_wasm
+
+
---
+++ centos_pre_build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+yum install -y alsa-lib-devel
