c1633b02066141cd4be7f3a6c766fb539dc0085d
Matthew Pfeiffer <spferical@gmail.com>
2025-01-19 17:37:40 -0500

    Remove nonworking gen-all command

--- server/main.py
+++ server/main.py
@@ -17,29 +17,6 @@ def cli():
 
 
 @cli.command()
-@click.argument("theme")
-@click.option("--initial-state-path", default=None)
-@click.option("--output-dir", default=None)
-def gen_all(theme: str, initial_state_path: str | None, output_dir: str | None):
-    if initial_state_path is not None:
-        with open(initial_state_path) as f:
-            init = json.load(f)
-            assert init["theme"] == theme
-            state = game_types.GameState(**init)
-    else:
-        state = game_types.GameState(theme=theme)
-    instruction = (
-        "Generate everything" if not initial_state_path else "Generate everything else"
-    )
-    ai.gen_anything(instruction, state)
-    print(state.model_dump_json(exclude_defaults=True))
-    if output_dir is not None:
-        os.makedirs(output_dir, exist_ok=True)
-        with open(os.path.join(output_dir, "game.json"), "w") as f:
-            f.write(state.model_dump_json(exclude_defaults=True))
-
-
-@cli.command()
 @click.argument("instruction")
 def gen_actions(instruction: str):
     state = game_types.GameState(**json.load(sys.stdin))
