fce476bd690995f774dd5bb62a1cc58bd3ee220a
Matthew Pfeiffer <spferical@gmail.com>
2025-03-11 22:02:45 -0400

    Add script to package client zips

---
+++ package-zips.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1;
+fi
+version=$1
+
+rm -f ./everythingrl-*-"${version}".zip
+(cd dist_windows && zip -r "../everythingrl-windows-${version}.zip" *)
+(cd dist_linux && zip -r "../everythingrl-linux-${version}.zip" *)
+(cd dist && zip -r "../everythingrl-web.zip" *)
+(cd dist_windows && zip -r "../everythingrl-windows-${version}.zip" *)
